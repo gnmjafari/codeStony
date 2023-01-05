d06f6161468316116d09e82f3a27a6637360d829
Ali jafari <gnm.jafaria@gmail.com>
2023-01-05 11:17:08 +0330

    edit About

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./component/about";
+import About from "./component/About";
 import Dashboard from "./component/Dashboard";
 import { Provider } from "react-redux";
 import store from "./component/Store/store";
