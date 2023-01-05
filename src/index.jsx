04a0c734e908c7369a14628e611397261421cce3
Ali jafari <gnm.jafaria@gmail.com>
2023-01-05 11:08:55 +0330

    edit index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./component/about";
 import Dashboard from "./component/Dashboard";
@@ -7,25 +8,22 @@ import store from "./component/Store/store";
 import ContactUs from "./component/ContactUs";
 import Singuplogin from "./component/Singuplogin";
 import Menu from "./component/Menu";
+import App from "./App";
 
-function index() {
-  return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Menu />
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/Home" element={<App />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/ContactUs" element={<ContactUs />} />
-            <Route path="/Singuplogin" element={<Singuplogin />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
-  );
-}
-
-export default index;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Home" element={<App />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Singuplogin" element={<Singuplogin />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./component/about";
-import Dashboard from "./component/Dashboard";
-import { Provider } from "react-redux";
-import store from "./component/Store/store";
-import ContactUs from "./component/ContactUs";
-import Singuplogin from "./component/Singuplogin";
-import Menu from "./component/Menu";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <index />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <index />
+//   </React.StrictMode>
+// );
