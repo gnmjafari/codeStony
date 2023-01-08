a26cbd9fc14ffa4831de86293a9c3b91b9ac770b
Ali jafari <gnm.jafaria@gmail.com>
2023-01-08 16:52:24 +0330

    Add login or Rergister

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,11 +1,76 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 function LoginForm() {
   return (
-    <Typography variant="h4" sx={{ margin: "100px" }}>
-      Coming Soon ... Login
-    </Typography>
+    <Box
+      style={{
+        backgroundColor: "#282A31",
+        height: "100Vh",
+        paddingTop: "150px",
+      }}
+    >
+      <Box
+        style={{
+          backgroundColor: "#fff",
+          width: "80%",
+          margin: "auto",
+          padding: "20px",
+          borderRadius: "20px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "60px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h3"
+          sx={{
+            color: "#282A31",
+            padding: "20px",
+            borderRadius: "20px",
+            fontWeight: "600",
+          }}
+        >
+          Code Stony
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "wrap",
+            gap: "40px",
+          }}
+        >
+          <TextField
+            sx={{ width: "100%" }}
+            label="Username Or Email"
+            type="username"
+          />
+          <TextField sx={{ width: "100%" }} label="Password" type="password" />
+        </Box>
+        <Button
+          variant="none"
+          sx={{
+            gap: "20px",
+            fontWeight: "600",
+            borderRadius: "20px",
+            backgroundColor: "#F8CE46",
+            color: "#282A31",
+            fontSize: "30px",
+            padding: "10px 30px",
+            ":hover": {
+              backgroundColor: "#F8CE46",
+            },
+          }}
+        >
+          ورود
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
--- src/component/RegisterForm.jsx
+++ src/component/RegisterForm.jsx
@@ -1,12 +1,79 @@
-import { Button, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
 
 function RegisterForm() {
   return (
     <>
-      <Typography variant="h4" sx={{ margin: "100px" }}>
-        Coming Soon ... Register
-      </Typography>
+      <Box
+        style={{
+          backgroundColor: "#282A31",
+          height: "100Vh",
+          paddingTop: "150px",
+        }}
+      >
+        <Box
+          style={{
+            backgroundColor: "#fff",
+            width: "80%",
+            margin: "auto",
+            padding: "20px",
+            borderRadius: "20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "60px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="h3"
+            sx={{
+              color: "#282A31",
+              padding: "20px",
+              borderRadius: "20px",
+              fontWeight: "600",
+            }}
+          >
+            Code Stony
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+              gap: "40px",
+            }}
+          >
+            <TextField sx={{ width: "40%" }} label="Username" type="username" />
+            <TextField sx={{ width: "40%" }} label="email" type="email" />
+            <TextField sx={{ width: "40%" }} label="Password" type="password" />
+            <TextField
+              sx={{ width: "40%" }}
+              label="agian password"
+              type="password"
+            />
+          </Box>
+          <Button
+            variant="none"
+            sx={{
+              gap: "20px",
+              fontWeight: "600",
+              borderRadius: "20px",
+              backgroundColor: "#F8CE46",
+              color: "#282A31",
+              fontSize: "30px",
+              padding: "10px 30px",
+              ":hover": {
+                backgroundColor: "#F8CE46",
+              },
+            }}
+          >
+            ثبت نام
+          </Button>
+        </Box>
+      </Box>
     </>
   );
 }
